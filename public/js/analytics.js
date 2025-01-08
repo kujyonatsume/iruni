3b8bd58feeed30e56df7382290a71c3845ae6ce9
kujyonatsume <adam200134@gmail.com>
2025-01-08 10:43:25 +0800

    加入 analytics 分析

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -3,3 +3,10 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-GELZQM159V');
+
+gtag('consent', 'default', {
+    'ad_storage': 'denied',
+    'ad_user_data': 'denied',
+    'ad_personalization': 'denied',
+    'analytics_storage': 'denied'
+  });
