af6c94230828649e94c3a3c4fb54633b1a3e5335
kujyonatsume <adam200134@gmail.com>
2025-01-16 18:07:23 +0800

    OK

--- pages/lyric-editor.vue
+++ pages/lyric-editor.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { mdiPlay, mdiRewind, mdiFastForward, mdiUpdate, mdiDelete, mdiPlus, mdiContentCopy, mdiMenuUp, mdiMenuDown } from "@mdi/js"
 useTitle().set("歌曲打軸");
-/** @type {Ref<HTMLAudioElement>} type - description */
-const audio = ref();
+/** @type {Ref<HTMLVideoElement>} type - description */
+const video = ref();
 const edittxt = ref(true);
 const lyricsList = ref([]);
-
+onMounted(()=> {
+    video.value = new HTMLVideoElement()
+    video.value
+})
 const timeRegex = /\[(\d+):(\d+\.\d+)\]/
 const obj = ref({ title: "", videoId: "", lyric: "" })
 // 音樂上傳功能
@@ -14,12 +17,13 @@ function uploadAudio(event) {
     const e = event.target
     const file = e.files[0];
     if (file) {
+        file.type
         const nameMatch = file.name.match(/(?:\d+-\d+) (.*?) \[(.*?)\]/)
-        obj.value.title = nameMatch[1]
-        obj.value.videoId = nameMatch[2]
+        obj.value.title = nameMatch?.[1] ?? ""
+        obj.value.videoId = nameMatch?.[2] ?? ""
         const url = URL.createObjectURL(file);
-        audio.value.src = url;
-        audio.value.play();
+        video.value.src = url;
+        video.value.play();
     }
 };
 
@@ -91,8 +95,7 @@ function sortLyricsList() {
 <template>
     <v-container>
         <v-file-input label="上傳音樂" accept="audio/*,video/*" @change="uploadAudio" />
-        <audio :style="{ margin: '2px' }" style="width: 100%;" ref="audio" controls> 您的瀏覽器不支援 audio 標籤</audio>
-
+        <input type="range" class="progress-bar" v-model="video.currentValue" :max="max" :min="min" @input="onInput" />
         <div style="display: flex;justify-content: space-between; justify-items: center;">
             <div :style="{ margin: '2px' }">
                 <v-btn :style="{ margin: '2px' }" @click="insertEmptyLine"><v-icon>{{ mdiPlus }}</v-icon></v-btn>
@@ -107,20 +110,28 @@ function sortLyricsList() {
             rows="20" outlined />
         <div v-else class="lyrics-container">
             <div style="display: flex;" v-for="(item, index) in lyricsList" :key="index">
-                <v-btn :style="{ margin: '2px' }" @click="playAtTime(item.time)" color="primary"><v-icon>{{ mdiPlay }}</v-icon></v-btn>
-                <v-btn :style="{ margin: '2px' }" @click="setTime(index, undefined)" color="primary"><v-icon>{{ mdiUpdate }}</v-icon></v-btn>
+                <v-btn :style="{ margin: '2px' }" @click="playAtTime(item.time)" color="primary"><v-icon>{{ mdiPlay
+                        }}</v-icon></v-btn>
+                <v-btn :style="{ margin: '2px' }" @click="setTime(index, undefined)" color="primary"><v-icon>{{
+                        mdiUpdate }}</v-icon></v-btn>
                 <div style="display: flex;justify-content: left;">
-                    <input :style="{ margin: '2px' }" @change="sortLyricsList" v-model="item.time" style="width: 100px; text-align: center;" />
-                    <div :style="{ margin: '2px', width: '50px' }" style="display: flex; flex-direction: column; align-content: center;">
-                        <v-btn :min-width="'50px'" :style="{ height: '50%', width: '50px' }" @click="updateTime(index, +0.5)" color="primary"><v-icon>{{ mdiMenuUp }}</v-icon></v-btn>
-                        <v-btn :min-width="'50px'" :style="{ height: '50%', width: '50px' }" @click="updateTime(index, -0.5)" color="primary"><v-icon>{{ mdiMenuDown }}</v-icon></v-btn>
+                    <input :style="{ margin: '2px' }" @change="sortLyricsList" v-model="item.time"
+                        style="width: 100px; text-align: center;" />
+                    <div :style="{ margin: '2px', width: '50px' }"
+                        style="display: flex; flex-direction: column; align-content: center;">
+                        <v-btn :min-width="'50px'" :style="{ height: '50%', width: '50px' }"
+                            @click="updateTime(index, +0.5)" color="primary"><v-icon>{{ mdiMenuUp }}</v-icon></v-btn>
+                        <v-btn :min-width="'50px'" :style="{ height: '50%', width: '50px' }"
+                            @click="updateTime(index, -0.5)" color="primary"><v-icon>{{ mdiMenuDown }}</v-icon></v-btn>
                     </div>
                 </div>
                 <input :style="{ margin: '2px' }" @change="sortLyricsList" v-model="item.lyrics" style="width: 100%;" />
-                <v-btn :style="{ margin: '2px' }" @click="deleteLine(index)" color="error"><v-icon>{{ mdiDelete }}</v-icon></v-btn>
+                <v-btn :style="{ margin: '2px' }" @click="deleteLine(index)" color="error"><v-icon>{{ mdiDelete
+                        }}</v-icon></v-btn>
             </div>
         </div>
-        <v-btn :style="{ margin: '2px' }" color="info" @click="edittxt = !edittxt">{{ edittxt ? '切換到編輯模式' : '切換到文本' }}</v-btn>
+        <v-btn :style="{ margin: '2px' }" color="info" @click="edittxt = !edittxt">{{ edittxt ? '切換到編輯模式' : '切換到文本'
+            }}</v-btn>
         <v-btn :style="{ margin: '2px' }" color="info" @click="copy"><v-icon>{{ mdiContentCopy }}</v-icon></v-btn>
     </v-container>
 
